b071a4de6bb897e15ae42c697412b45535c4f6ad
Francesco Bognini <info@francescobognini.com>
2024-12-05 03:47:36 +0100

    Use require and not require_one

--- init.php
+++ init.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once __DIR__ . '/lib/Api.php';
-require_once __DIR__ . '/lib/NotSuccessException.php';
-require_once __DIR__ . '/lib/Request.php';
-require_once __DIR__ . '/lib/PaymentHelper.php';
+require __DIR__ . '/lib/Api.php';
+require __DIR__ . '/lib/NotSuccessException.php';
+require __DIR__ . '/lib/Request.php';
+require __DIR__ . '/lib/PaymentHelper.php';
 
-require_once __DIR__ . '/lib/Payment/Payment.php';
-require_once __DIR__ . '/lib/Payment/CreatePayment/CreatePaymentRequest.php';
-require_once __DIR__ . '/lib/Payment/CreatePayment/CreatePaymentRequestDiscount.php';
-require_once __DIR__ . '/lib/Payment/CreatePayment/CreatePaymentRequestLine.php';
+require __DIR__ . '/lib/Payment/Payment.php';
+require __DIR__ . '/lib/Payment/CreatePayment/CreatePaymentRequest.php';
+require __DIR__ . '/lib/Payment/CreatePayment/CreatePaymentRequestDiscount.php';
+require __DIR__ . '/lib/Payment/CreatePayment/CreatePaymentRequestLine.php';
 
-require_once __DIR__ . '/lib/PaymentGateway/PaymentGateway.php';
+require __DIR__ . '/lib/PaymentGateway/PaymentGateway.php';
+
